731497087aac160fc4d7020769f50cd3bb1923e1
Admin <ponomarenko02041993@gmail.com>
2019-04-27 00:28:58 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ $(document).ready(function () {
    $('.btn-drop-down').click(function () {
       $('.list-db').toggleClass('active-lg-db');
    });
+    (function($){
+        $(window).on("load",function(){
+
+            $("tbody").mCustomScrollbar({
+                theme:"minimal"
+            });
+
+        });
+    })(jQuery);
 });
