2d17fe732a080a876a4013d78dd27e56bbf1dd42
Admin <ponomarenko02041993@gmail.com>
2019-04-30 16:29:16 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
    $('.btn-drop-down').click(function () {
       $('.list-db').toggleClass('active-lg-db');
    });
+
+   /*---------------------scroll-----------------*/
+
     (function($){
         $(window).on("load",function(){
 
@@ -11,4 +14,43 @@ $(document).ready(function () {
 
         });
     })(jQuery);
+
+
+    /*--------------------------modal window-------------------*/
+    $("#modal-text").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text2").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text3").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text4").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text5").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text6").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text7").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text8").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text9").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text10").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text11").flythat({
+        fadeIn: 'slow'
+    });
+    $("#modal-text12").flythat({
+        fadeIn: 'slow'
+    });
 });
