000c070bd7adb58da61933f044c5a28ce9449548
Admin <ponomarenko02041993@gmail.com>
2019-06-03 09:24:49 +0300

    new

--- js/main.js
+++ js/main.js
@@ -87,4 +87,18 @@ $('.btn-drop-down').click(function () {
 	 	$(this).closest('.landing-header').find('.mobile-wrapper-menu-landing').slideToggle();
 
 	 });
+
+    $(".list-nav").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
